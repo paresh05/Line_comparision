68a480e788b4c444463d59098aef2285ff768496
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-19 12:31:19 +0530

    uc2 code

--- Linecomp.java
+++ Linecomp.java
@@ -6,6 +6,7 @@ public class Linecomp {
 
 	public static void main(String[] args) {
 	
+		// code for line 1
 		System.out.println("Welcome to Line Comparision Computation Program");
 		Scanner sc = new Scanner(System.in);  
 	    System.out.println("Enter the value of x1");
@@ -23,9 +24,34 @@ public class Linecomp {
 	    
 	    int x3=(x2-x1);
 	    int y3=(y2-y1);
-	    double lengthOfaLine=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
+	    Double lengthOfaLine1=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
 	  
-	    System.out.println("Length of line is: "+lengthOfaLine);
+	    System.out.println("Length of line 1 is: "+lengthOfaLine1);
+	    
+	    
+	 // code for line 2
+	 		System.out.println("Welcome to Line Comparision Computation Program");
+	 	    System.out.println("Enter the value of a1");
+	 	    int a1 = sc.nextInt();  
+	 	    System.out.println("x1 is: " + a1);
+	 	    System.out.println("Enter the value of b1");
+	 	    int b1 = sc.nextInt();  
+	 	    System.out.println("y1 is: " + b1);
+	 	    System.out.println("Enter the value of a2");
+	 	    int a2 = sc.nextInt();  
+	 	    System.out.println("x2 is: " + a2);
+	 	    System.out.println("Enter the value of b2");
+	 	    int b2 = sc.nextInt();  
+	 	    System.out.println("y2 is: " + b2);
+	 	    
+	 	    int a3=(a2-a1);
+	 	    int b3=(b2-b1);
+	 	    Double lengthOfaLine2=Math.sqrt((Math.pow(a3, 2))+(Math.pow(b3, 2)));
+	 	  
+	 	    System.out.println("Length of line 2 is: "+lengthOfaLine2);
+	 	    
+	 	   if(lengthOfaLine1.equals(lengthOfaLine2))
+		    	System.out.println("Length of 2 Lines are equal");
 		
 	}
 
