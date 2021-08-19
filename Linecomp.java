ec47503ac174c9fa646f7f4541831b5ef14afa46
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-19 12:01:54 +0530

    master code

---
+++ Linecomp.java
@@ -0,0 +1,11 @@
+package com.bridgelabz2;
+
+public class Linecomp {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to Line Comparision Computation Program");
+		
+	}
+
+}
