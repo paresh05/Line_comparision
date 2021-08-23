3bdd4ded0d7de5ac7981960e7fb2484a6526576d
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-23 11:01:57 +0530

    Added OOPS concepts of Line

--- Linecomp.java
+++ Linecomp.java
@@ -2,61 +2,82 @@ package com.bridgelabz2;
 import java.util.Scanner;
 import java.lang.Math;
 
+class Line
+{
+	int x1,y1,x2,y2;
+	int a1,b1,a2,b2;
+	
+	//Calculates the length of the line
+	public Double LengthofLine(int p3,int q3)
+	{
+		Double lengthOfaLine=Math.sqrt((Math.pow(p3, 2))+(Math.pow(q3, 2)));	
+		return lengthOfaLine;
+	}
+	
+	// Compares the length of the line
+	public void Compare(Double lengthOfaLine1,Double lengthOfaLine2)
+	{
+		int b=(lengthOfaLine1.compareTo(lengthOfaLine2));
+	 	if(b<0)
+	 		System.out.println("Length of Line 2 is greater than Line 1");
+	 	else if (b>0)
+	 		System.out.println("Length of Line 1 is greater than Line 2");
+	 	else if(b==0)
+	 		System.out.println("Length of 2 Lines are equal");
+	}
+		
+}
+
 public class Linecomp {
 
 	public static void main(String[] args) {
-	
+		//Object Creation
+		Line obj1=new Line();
 		// code for line 1
 		System.out.println("Welcome to Line Comparision Computation Program");
 		Scanner sc = new Scanner(System.in);  
 	    System.out.println("Enter the value of x1");
-	    int x1 = sc.nextInt();  
-	    System.out.println("x1 is: " + x1);
+	    obj1.x1 = sc.nextInt();  
+	    System.out.println("x1 is: " + obj1.x1);
 	    System.out.println("Enter the value of y1");
-	    int y1 = sc.nextInt();  
-	    System.out.println("y1 is: " + y1);
+	    obj1.y1 = sc.nextInt();  
+	    System.out.println("y1 is: " + obj1.y1);
 	    System.out.println("Enter the value of x2");
-	    int x2 = sc.nextInt();  
-	    System.out.println("x2 is: " + x2);
+	    obj1.x2 = sc.nextInt();  
+	    System.out.println("x2 is: " + obj1.x2);
 	    System.out.println("Enter the value of y2");
-	    int y2 = sc.nextInt();  
-	    System.out.println("y2 is: " + y2);
+	    obj1.y2 = sc.nextInt();  
+	    System.out.println("y2 is: " + obj1.y2);
 	    
-	    int x3=(x2-x1);
-	    int y3=(y2-y1);
-	    Double lengthOfaLine1=Math.sqrt((Math.pow(x3, 2))+(Math.pow(y3, 2)));
+	    int x3=(obj1.x2-obj1.x1);
+	    int y3=(obj1.y2-obj1.y1);
+	    Double lengthOfaLine1=obj1.LengthofLine(x3, y3);
 	  
 	    System.out.println("Length of line 1 is: "+lengthOfaLine1);
+	   
+	    // code for line 2
 	    
-	    
-	 // code for line 2
-	 	    System.out.println("Enter the value of a1");
-	 	    int a1 = sc.nextInt();  
-	 	    System.out.println("x1 is: " + a1);
-	 	    System.out.println("Enter the value of b1");
-	 	    int b1 = sc.nextInt();  
-	 	    System.out.println("y1 is: " + b1);
-	 	    System.out.println("Enter the value of a2");
-	 	    int a2 = sc.nextInt();  
-	 	    System.out.println("x2 is: " + a2);
-	 	    System.out.println("Enter the value of b2");
-	 	    int b2 = sc.nextInt();  
-	 	    System.out.println("y2 is: " + b2);
+	 	System.out.println("Enter the value of a1");
+	 	obj1.a1 = sc.nextInt();  
+	    System.out.println("x1 is: " + obj1.a1);
+	    System.out.println("Enter the value of b1");
+	    obj1.b1 = sc.nextInt();  
+ 	    System.out.println("y1 is: " + obj1.b1);
+ 	    System.out.println("Enter the value of a2");
+	    obj1.a2 = sc.nextInt();  
+	    System.out.println("x2 is: " + obj1.a2);
+	    System.out.println("Enter the value of b2");
+	    obj1.b2 = sc.nextInt();  
+	    System.out.println("y2 is: " + obj1.b2);
 	 	    
-	 	    int a3=(a2-a1);
-	 	    int b3=(b2-b1);
-	 	    Double lengthOfaLine2=Math.sqrt((Math.pow(a3, 2))+(Math.pow(b3, 2)));
+ 	    int a3=(obj1.a2-obj1.a1);
+ 	    int b3=(obj1.b2-obj1.b1);
+	    Double lengthOfaLine2=obj1.LengthofLine(a3, b3);
 	 	  
-	 	    System.out.println("Length of line 2 is: "+lengthOfaLine2);
-	 	    
-	 	   int b=(lengthOfaLine1.compareTo(lengthOfaLine2));
-	 	   if(b<0)
-	 		   System.out.println("Length of Line 2 is greater than Line 1");
-	 	   else if (b>0)
-	 		  System.out.println("Length of Line 1 is greater than Line 2");
-	 	   else if(b==0)
-	 		  System.out.println("Length of 2 Lines are equal");
-		
+ 	    System.out.println("Length of line 2 is: "+lengthOfaLine2);
+	 	//Function call to compare funtion    
+	    obj1.Compare(lengthOfaLine1, lengthOfaLine2);
+	 	    		
 	}
 
 }
